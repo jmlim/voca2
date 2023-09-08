9b713cd6914bb243780a3c9a554435d1b7b0e736
jmlim <hackerljm@gmail.com>
2023-09-08 23:13:31 +0900

    Word 컴포넌트 및 토글추가.

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,3 +1,4 @@
+import Word from "./Word";
 import dummy from "./db/data.json";
 import { useParams } from "react-router-dom";
 
@@ -16,10 +17,7 @@ export default function Day() {
       <table>
         <tbody>
           {wordList.map((word) => (
-            <tr key={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+            <Word word={word} key={word.id}></Word>
           ))}
         </tbody>
       </table>
