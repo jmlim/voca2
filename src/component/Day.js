034aac44479f5e5aff316d3159df93561145b5fd
jmlim <hackerljm@gmail.com>
2023-09-09 00:30:38 +0900

    useEffect 및 fetch 를 이용한 데이터 조회 후 렌더링

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,22 +1,27 @@
+import { useEffect, useState } from "react";
 import Word from "./Word";
-import dummy from "./db/data.json";
 import { useParams } from "react-router-dom";
 
 export default function Day() {
-  //dummy.worlds
   const { day } = useParams();
-  const wordList = dummy.words.filter((word) => {
-    return word.day === Number(day);
-  });
-  console.log(day);
-  console.log(wordList);
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((words) => {
+        setWords(words);
+      });
+  }, [day]);
 
   return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map((word) => (
+          {words.map((word) => (
             <Word word={word} key={word.id}></Word>
           ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -21,7 +21,14 @@ export default function DayList() {
   // 어떤 상태값이 바뀌었을 때 동작하는 이벤트
   // 렌더링 결과가 dom 에 반영된 직후
   useEffect(() => {
-    console.log("Count change");
+    // 이렇게 하면 json으로 변환 후 promise 를 리턴한다.
+    fetch("http://localhost:3001/days")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setDays(data);
+      });
   }, []);
   // 2번째 인자는 의존성 배열이라고 함.
   // 특정 변수가 변경 되었을 시 호출하려면 의존성 배열에 상태변수를 넣음
